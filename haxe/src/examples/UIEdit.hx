e8afa4cb6d9fa1644eeae14718576d9341baf3bb
Peter "sHTiF" Stefcek <pshtif@gmail.com>
2015-01-31 22:59:56 +0100

    refactoring 1.1

--- haxe/src/examples/UIEdit.hx
+++ haxe/src/examples/UIEdit.hx
@@ -93,16 +93,11 @@ class UIEdit {
                                     '</element>'+
                                 '</def>';
 
-    private var _bottomMenuPrototype:String = '<element>'+
+    private var _bottomMenuPrototype:String = '<element anchorY="200">'+
                                                 '<horizontal/>'+
-                                                '<element skinId="ui_btn_bottom"><element skinId="uiFont">Back</element></element>'+
-                                                '<element skinId="ui_btn_bottom_map"></element>'+
-                                                '<element skinId="ui_btn_bottom_map"></element>'+
-                                                '<element skinId="ui_btn_bottom_map"></element>'+
-                                                //'<element name="Quest" mouseDown="questDownHandler" skinId="ui_btn_bottom"><element skinId="uiFont">Quests</element></element>'+
                                               '</element>';
 
-    private var elementDef:String = '<element mouseDown="test" skinId="xpSkin"/>';
+    private var elementDef:String = '<element name="Quest" mouseDown="questDownHandler" skinId="ui_btn_bottom"><element name="label" skinId="uiFont">Quests</element></element>';
     private var skinDef:String = '<fontSkin id="font" fontAtlasId="font_ui.png" autoSize="0"/>';
 
     public function test(signal:GUIMouseSignal):Void {
@@ -111,6 +106,9 @@ class UIEdit {
 
     private function initExample():Void {
         trace("initExample");
+        var a = [{a:"aaaa"}];
+        trace(Type.typeof(a));
+        trace(Std.is(a,Array));
         GAssetManager.generateTextures();
         var a:GUITextureSkin;
         var f:GUIFontSkin;
@@ -131,73 +129,23 @@ class UIEdit {
         new GUITextureSkin("ui_btn_bottom_map", "ui.png_btn_bottom_map");
         new GUIFontSkin("uiFont", "font_ui.png");
 
-        var element:GUIElement = cast GPrototypeFactory.createPrototype(Xml.parse(_bottomMenuPrototype).firstElement());
+        var element:GUIElement = cast GPrototypeFactory.createPrototype(_bottomMenuPrototype);
         ui.root.addChild(element);
 
-        /*
-        var element:GUIElement = null;
-        var xml:Xml = Xml.parse(xmlDef).firstChild();
-        for (i in xml.elements()) {
-            var p:IGPrototypable = GPrototypeFactory.createPrototype(i);
-            trace(p.getPrototype());
-            if (Std.is(p,GUIElement)) {
-                element = cast p;
-                element.name = "container";
-                element.mouseEnabled = true;
-                element.anchorX = 300;
-                element.anchorY = 300;
-                ui.root.addChild(element);
+        element.listItemPrototype = Xml.parse('<element name="Quest" mouseDown="questDownHandler" skinId="ui_btn_bottom"><element name="label" skinId="uiFont">Quests</element></element>').firstElement();
+        element.setValue([{label:"test"},{}]);
 
-                element.setClient(this);
-            }
-        }
+        var element2:GUIElement = cast GPrototypeFactory.createPrototype(_bottomMenuPrototype);
+        element2.anchorY = 400;
+        ui.root.addChild(element2);
 
-        //if (element != null) element.addChild(cast GPrototypeFactory.createPrototype(Xml.parse(elementDef).firstElement()));
-        /*
-        ui = cast GNode.createWithComponent(GUI);
-        ui.root.setRect(0,0,2038,1536);
-        ui.node.mouseEnabled = true;
-        genome.root.addChild(ui.node);
-
-        var scrollContainer:GUIElement = new GUIElement();
-        scrollContainer.id = "container";
-        scrollContainer.mouseEnabled = true;
-        scrollContainer.onMouseDown.add(mouseDownHandler);
-        ui.root.addChild(scrollContainer);
-
-            var skin:GUITextureSkin = new GUITextureSkin();
-            skin.sliceLeft = 43;
-            skin.sliceRight = 86;
-            skin.sliceTop = 43;
-            skin.sliceBottom = 86;
-            //skin.textureId = "ui.png_img_shop_card_cleen";
-            skin.textureId = "Untitled.png";
-
-            var card:GUIElement = new GUIElement();
-            card.normalSkin = skin;
-        card.preferredWidth = 300;
-        card.preferredHeight = 300;
-            scrollContainer.addChild(card);
-
-            var image:GUIElement = new GUIElement();
-            image.normalSkin = new GUITextureSkin();
-            image.anchorTop = image.anchorBottom = .3;
-            card.addChild(image);
-        /*
-        rectGizmo = new RectGizmo();
-        Lib.current.addChild(rectGizmo);
-
-        anchorGizmo = new AnchorGizmo();
-        Lib.current.addChild(anchorGizmo);
-
-        treelist = new TreeList();
-        treelist.parseData(ui.root);
-        treelist.x = 0;
-        treelist.y = 300;
-        treelist.onSelect.add(selectHandler);
-        genome.getContext().getNativeStage().addChild(treelist);
-        /**/
+        trace(element.getPrototype());
+        element2.listItemPrototype = element.getPrototype();
+        ui.root.addChild(element2);
+        element2.setValue([{},{}]);
     }
+
+    private var test2:String = '<element anchorX="0" anchorY="200" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="GUIElement" skinId="" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="null" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"><horizontal gap="10" type="2"/><element anchorX="0" anchorY="0" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="Quest" skinId="ui_btn_bottom" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="questDownHandler" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"><element anchorX="0" anchorY="0" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="label" skinId="uiFont" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="null" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"/></element><element anchorX="0" anchorY="0" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="Quest" skinId="ui_btn_bottom" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="questDownHandler" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"><element anchorX="0" anchorY="0" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="label" skinId="uiFont" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="null" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"/></element></element>';
 
     private function mouseDownHandler(signal:GUIMouseSignal):Void {
         trace(signal.target, signal.target.skin!=null?signal.target.skinId:"");
