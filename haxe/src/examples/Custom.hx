74d4597079d870d45200dd5721467431d2972aed
sHTiF <pshtif@gmail.com>
2015-02-05 17:47:17 +0100

    refactoring 1.1

--- haxe/src/examples/Custom.hx
+++ haxe/src/examples/Custom.hx
@@ -1,5 +1,13 @@
 package examples;
 
+import com.genome2d.tilemap.GTile;
+import com.genome2d.signals.GKeyboardSignal;
+import com.genome2d.signals.GKeyboardSignalType;
+import com.genome2d.signals.GMouseSignalType;
+import com.genome2d.signals.GMouseSignal;
+import com.genome2d.context.GBlendMode;
+import com.genome2d.tilemap.GTile;
+import com.genome2d.components.renderable.tilemap.GTileMap;
 import flash.geom.Vector3D;
 import com.genome2d.geom.GMatrix3D;
 import flash.display.BitmapData;
@@ -67,6 +75,25 @@ class Custom {
         trace("initAssets");
 
         GAssetManager.init();
+        GAssetManager.addFromUrl("terrain\\0.png");
+        GAssetManager.addFromUrl("terrain\\01.png");
+        GAssetManager.addFromUrl("terrain\\02.png");
+        GAssetManager.addFromUrl("terrain\\03.png");
+        GAssetManager.addFromUrl("terrain\\04.png");
+        GAssetManager.addFromUrl("terrain\\10.png");
+        GAssetManager.addFromUrl("terrain\\1t.png");
+        GAssetManager.addFromUrl("terrain\\1l.png");
+        GAssetManager.addFromUrl("terrain\\1b.png");
+        GAssetManager.addFromUrl("terrain\\1r.png");
+        GAssetManager.addFromUrl("terrain\\2t.png");
+        GAssetManager.addFromUrl("terrain\\2l.png");
+        GAssetManager.addFromUrl("terrain\\2b.png");
+        GAssetManager.addFromUrl("terrain\\2r.png");
+        GAssetManager.addFromUrl("terrain\\31.png");
+        GAssetManager.addFromUrl("terrain\\32.png");
+        GAssetManager.addFromUrl("terrain\\33.png");
+        GAssetManager.addFromUrl("terrain\\34.png");
+        GAssetManager.addFromUrl("uniformclouds.jpg");
         GAssetManager.addFromUrl("Untitled.png");
         GAssetManager.addFromUrl("ui.png");
         GAssetManager.addFromUrl("ui.xml");
@@ -88,31 +115,199 @@ class Custom {
 
     private var renderer:GCustomRenderer;
     private var texture:GTexture;
+    private var buildings:Array<Float>;
+    private var map:GTileMap;
+    private var mapIds:Array<Int> = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,13,0,1,1,1,1,1,1,1,1,1,1,1,1,13,0,0,1,1,1,1,1,1,1,1,1,1,1,1,13,0,0,1,1,1,1,1,1,1,1,1,1,1,13,0,0,0,1,1,1,1,1,1,1,1,1,1,1,13,0,0,0,1,1,1,1,1,1,1,1,1,1,13,0,0,0,0,1,1,1,1,1,1,1,1,1,1,13,0,0,0,0,1,1,1,1,1,1,1,1,1,13,0,0,0,0,0,1,1,1,1,1,1,1,1,1,13,0,0,0,0,0,1,1,1,1,1,1,1,1,13,0,0,0,0,0,0,1,1,1,1,1,1,1,1,13,0,0,0,0,0,0,1,1,1,1,1,1,1,13,0,0,0,0,0,0,0,1,1,1,1,1,1,1,13,0,0,0,0,0,0,0,1,1,1,1,1,1,13,0,0,0,0,0,0,0,0,1,1,1,1,1,1,13,0,0,0,0,0,0,0,0,0,1,1,1,1,13,0,0,0,0,0,0,0,0,0,0,1,1,1,1,13,0,0,0,0,0,0,0,0,0,0,1,1,1,13,0,0,0,0,0,0,0,0,0,0,0,1,1,1,13,0,0,0,0,0,0,0,0,0,0,0,1,1,13,0,0,0,0,0,0,0,0,0,0,0,0,1,1,13,0,0,0,0,0,0,0,0,0,0,0,0,1,13,0,0,0,0,0,0,0,0,0,0,0,0,1,1,13,0,0,0,0,0,0,0,0,0,0,0,0,0,13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    private var textureId:String = "terrain\\0.png";
 
     private function initExample():Void {
+    trace(mapIds.length);
         trace("initExample");
         GAssetManager.generateTextures();
 
+        var w:Int = 15;
+        var h:Int = 40;
+        var tiles:Array<GTile> = new Array<GTile>();
+        for (i in 0...w*h) {
+            var tile:GTile = new GTile();
+            setTile(tile, mapIds[i]);//"terrain\\10.png";
+            tiles.push(tile);
+        }
+
+        map = cast GNode.createWithComponent(GTileMap);
+        map.setTiles(w,h,64,34,tiles,true);
+        map.node.x = 400;
+        map.node.y = 250;
+        map.verticalMargin = 6;
+        genome.root.addChild(map.node);
+
         texture = GTextureManager.getTextureById("Untitled.png");
 
-        var w:Float = 50;
-        var h:Float = 50;
-        var d:Float = 50;
+        var w:Float = 40;
+        var h:Float = 20;
+        var d:Float = 20;
+
+        buildings = new Array<Float>();
 
         //renderer = new GCustomRenderer([-w,-h,-d,w,-h,-d,-w,h,-d, -w, h, -d, w, -h, -d, w,h,-d],[0,0,1,0,0,1,1,0,0,1,1,1]);//,[0,1,2,2,1,3]);
-        renderer = new GCustomRenderer([-w,-h,-d,w,-h,-d,-w,h,-d, w,h,-d,-w,-h,d,w,-h,d,-w,h,d,w,h,d], [0,0,1,0,0,1,1,1,1,0,0,0,1,1,0,1], [0,1,2,2,1,3,5,4,7,7,4,6,3,1,7,7,1,5,2,3,6,6,3,7,0,2,4,4,2,6,1,0,5,5,0,4], true);
-        renderer.transformMatrix.appendTranslation(0,0,1000);
+        renderer = new GCustomRenderer([-w,-h,-d,w,-h,-d,-w,h,-d, w,h,-d,-w,-h,d,w,-h,d,-w,h,d,w,h,d], [0,0,1,0,0,1,1,1,1,0,0,0,1,1,0,1], [0,1,2,2,1,3,5,4,7,7,4,6,3,1,7,7,1,5,2,3,6,6,3,7,0,2,4,4,2,6,1,0,5,5,0,4], false);
 
         genome.onPostRender.add(postRenderHandler);
+        genome.getContext().onMouseSignal.add(mouseSignalHandler);
+        genome.getContext().onKeyboardSignal.add(keyboardSignalHandler);
+    }
+
+    private function keyboardSignalHandler(signal:GKeyboardSignal):Void {
+        trace(signal.keyCode);
+        if (signal.type != GKeyboardSignalType.KEY_DOWN) return;
+        switch (signal.keyCode) {
+            case 84:
+                traceMap();
+            case 48:
+                textureId = "terrain\\10.png";
+            case 49:
+                textureId = "terrain\\0.png";
+            case 50:
+                textureId = "terrain\\31.png";
+            case 51:
+                textureId = "terrain\\32.png";
+            case 52:
+                textureId = "terrain\\33.png";
+            case 53:
+                textureId = "terrain\\34.png";
+        }
+    }
+
+    private var mouseDown:Bool = false;
+    private var overTile:GTile;
+
+    private function mouseSignalHandler(signal:GMouseSignal):Void {
+        switch (signal.type) {
+            case GMouseSignalType.MOUSE_DOWN:
+                buildings.push(signal.x);
+                buildings.push(signal.y);
+                buildings.push(Math.random()*180);
+            /*
+                mouseDown = true;
+                var tile:GTile = map.getTileAt(signal.x, signal.y);
+                if (tile != null) tile.textureId = textureId;
+                /**/
+            case GMouseSignalType.MOUSE_MOVE:
+                /*
+                if (overTile != null) overTile.blue = 1;
+                overTile = map.getTileAt(signal.x, signal.y);
+                overTile.blue = 0;
+
+                if (mouseDown) {
+                    if (overTile!=null) overTile.textureId = textureId;
+                }
+                /**/
+            case GMouseSignalType.MOUSE_UP:
+                mouseDown = false;
+        }
     }
 
     private function postRenderHandler():Void {
         var context:IContext = genome.getContext();
 
-        renderer.transformMatrix.prependRotation(1,Vector3D.X_AXIS);
-        renderer.transformMatrix.prependRotation(1,Vector3D.Y_AXIS);
-
         context.bindRenderer(renderer);
-        renderer.draw(texture);
+        for (i in 0...Std.int(buildings.length/3)) {
+            buildings[i*3+2]+=1;
+            renderer.transformMatrix.identity();
+            renderer.transformMatrix.prependTranslation(0,0,50);
+            renderer.transformMatrix.prependRotation(32,Vector3D.X_AXIS);
+            renderer.transformMatrix.prependRotation(45+buildings[i*3+2],Vector3D.Y_AXIS);
+            renderer.transformMatrix.appendTranslation(buildings[i*3],buildings[i*3+1]+Math.sin(buildings[i*3+2]/20)*10,0);
+            renderer.draw(texture,1);
+
+            renderer.transformMatrix.identity();
+            renderer.transformMatrix.prependTranslation(0,0,50);
+            renderer.transformMatrix.prependRotation(32,Vector3D.X_AXIS);
+            renderer.transformMatrix.prependRotation(45+buildings[i*3+2],Vector3D.Y_AXIS);
+            renderer.transformMatrix.prependScale(1,-1,1);
+            renderer.transformMatrix.appendTranslation(buildings[i*3],buildings[i*3+1]+60-Math.sin(buildings[i*3+2]/20)*10,0);
+            renderer.draw(texture,2);
+        }
+
+        //context.draw(GTextureManager.getTextureById("uniformclouds.jpg"),400,300,1,1,0,1,1,1,.7,GBlendMode.SCREEN);
+    }
+
+    private function traceMap():Void {
+        var tiles:Array<GTile> = map.getTiles();
+        var s:String = "";
+        for (i in 0...tiles.length) {
+            s+=getTile(tiles[i])+",";
+        }
+        trace(s);
+    }
+
+    private function getTile(p_tile:GTile):Int {
+        var id:String = p_tile.textureId;
+        switch (id) {
+            case "terrain\\0.png":
+                return 0;
+            case "terrain\\10.png":
+                return 1;
+            case "terrain\\1t.png":
+                return 2;
+            case "terrain\\1b.png":
+                return 3;
+            case "terrain\\1l.png":
+                return 4;
+            case "terrain\\1r.png":
+                return 5;
+            case "terrain\\2t.png":
+                return 6;
+            case "terrain\\2b.png":
+                return 7;
+            case "terrain\\2l.png":
+                return 8;
+            case "terrain\\2r.png":
+                return 9;
+            case "terrain\\31.png":
+                return 10;
+            case "terrain\\32.png":
+                return 11;
+            case "terrain\\33.png":
+                return 12;
+            case "terrain\\34.png":
+                return 13;
+        }
+
+        return -1;
+    }
+
+    private function setTile(p_tile:GTile, p_value:Int):Void {
+        switch (p_value) {
+            case 0:
+                p_tile.textureId = "terrain\\0.png";
+                //p_tile.frameTextureIds = ["terrain\\01.png","terrain\\02.png","terrain\\03.png","terrain\\04.png"];
+            case 1:
+                p_tile.textureId = "terrain\\10.png";
+            case 2:
+                p_tile.textureId = "terrain\\1t.png";
+            case 3:
+                p_tile.textureId = "terrain\\1b.png";
+            case 4:
+                p_tile.textureId = "terrain\\1l.png";
+            case 5:
+                p_tile.textureId = "terrain\\1r.png";
+            case 6:
+                p_tile.textureId = "terrain\\2t.png";
+            case 7:
+                p_tile.textureId = "terrain\\2b.png";
+            case 8:
+                p_tile.textureId = "terrain\\2l.png";
+            case 9:
+                p_tile.textureId = "terrain\\2r.png";
+            case 10:
+                p_tile.textureId = "terrain\\31.png";
+            case 11:
+                p_tile.textureId = "terrain\\32.png";
+            case 12:
+                p_tile.textureId = "terrain\\33.png";
+            case 13:
+                p_tile.textureId = "terrain\\34.png";
+        }
     }
 }
--- haxe/src/examples/UIEdit.hx
+++ haxe/src/examples/UIEdit.hx
@@ -132,17 +132,8 @@ class UIEdit {
         var element:GUIElement = cast GPrototypeFactory.createPrototype(_bottomMenuPrototype);
         ui.root.addChild(element);
 
-        element.listItemPrototype = Xml.parse('<element name="Quest" mouseDown="questDownHandler" skinId="ui_btn_bottom"><element name="label" skinId="uiFont">Quests</element></element>').firstElement();
+        element.listItemPrototype = Xml.parse('<element name="Quest" mouseDown="mouseDownHandler" skinId="ui_btn_bottom"><element name="label" skinId="uiFont">Quests</element></element>').firstElement();
         element.setValue([{label:"test"},{}]);
-
-        var element2:GUIElement = cast GPrototypeFactory.createPrototype(_bottomMenuPrototype);
-        element2.anchorY = 400;
-        ui.root.addChild(element2);
-
-        trace(element.getPrototype());
-        element2.listItemPrototype = element.getPrototype();
-        ui.root.addChild(element2);
-        element2.setValue([{},{}]);
     }
 
     private var test2:String = '<element anchorX="0" anchorY="200" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="GUIElement" skinId="" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="null" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"><horizontal gap="10" type="2"/><element anchorX="0" anchorY="0" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="Quest" skinId="ui_btn_bottom" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="questDownHandler" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"><element anchorX="0" anchorY="0" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="label" skinId="uiFont" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="null" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"/></element><element anchorX="0" anchorY="0" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="Quest" skinId="ui_btn_bottom" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="questDownHandler" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"><element anchorX="0" anchorY="0" anchorLeft="0" anchorRight="0" anchorTop="0" anchorBottom="0" pivotX="0" pivotY="0" left="0" right="0" top="0" bottom="0" preferredWidth="0" preferredHeight="0" name="label" skinId="uiFont" mouseEnabled="true" visible="true" flushBatch="false" mouseDown="null" mouseUp="null" mouseClick="null" mouseOver="null" mouseOut="null" mouseMove="null"/></element></element>';
