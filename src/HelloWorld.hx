6a4e8076405d5100c36e55026d80e05bd6d33585
Peter @sHTiF Stefcek <pshtif@gmail.com>
2015-10-08 19:12:42 +0200

    update 1.1

--- src/HelloWorld.hx
+++ src/HelloWorld.hx
@@ -1,6 +1,13 @@
+typedef Test<T> = {
+	var x:T;
+	var y:T;
+}
+
 class HelloWorld
 {
 	static public function main():Void {
+		var a = {x:1.0, y:5.0};
+		var b:Test<Float> = a;
 		trace("Hello World");
 	}	
 }
