7d2d1cc5527238cbe741bc57423a5536ca4abae0
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-06-05 23:56:54 +0200

    update g3d example

--- src/com/genome2d/examples/G3DExample.hx
+++ src/com/genome2d/examples/G3DExample.hx
@@ -1,6 +1,7 @@
 package com.genome2d.examples;
-import com.genome2d.context.webgl.renderers.G3DRenderer;
+import com.genome2d.context.renderers.G3DRenderer;
 import com.genome2d.examples.AbstractExample;
+import com.genome2d.geom.GVector3D;
 import com.genome2d.textures.GTextureManager;
 
 /**
@@ -18,15 +19,15 @@ class G3DExample extends AbstractExample
         Initialize Example code
      **/
     override private function initExample():Void {
-		renderer = new G3DRenderer([100, 100, 0, 200, 100, 0, 100, 200, 0, 200, 200, 0], [0, 0, 1, 0, 0, 1, 1, 1], [0, 1, 2, 2, 1, 3]);
+		renderer = new G3DRenderer([100, 100, 0, 200, 100, 0, 100, 200, 0, 200, 200, 0], [0, 0, 1, 0, 0, 1, 1, 1], [0, 1, 2, 2, 1, 3], null);
 		renderer.texture = GTextureManager.getTexture("assets/texture");
 		
 		genome.onPostRender.add(postRender_handler);
 	}
-	
+
 	private function postRender_handler():Void {
-	
+		renderer.modelMatrix.appendRotation(1, GVector3D.Y_AXIS);
 		genome.getContext().setRenderer(renderer);
-		renderer.draw();
+		renderer.draw(0, 1);
 	}
 }
