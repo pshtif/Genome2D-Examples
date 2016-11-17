19e55be48cbfd5e25668a28931367089cbff9c0f
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-11-17 17:27:48 +0100

    update

--- src/com/genome2d/examples/ParticlesExample.hx
+++ src/com/genome2d/examples/ParticlesExample.hx
@@ -8,6 +8,8 @@
  */
 package com.genome2d.examples;
 
+import tween.Delta;
+import com.genome2d.particles.modules.GSPHVelocityModule;
 import com.genome2d.scripts.GScriptManager;
 import com.genome2d.scripts.GScript;
 import com.genome2d.geom.GRectangle;
@@ -27,8 +29,11 @@ import com.genome2d.node.GNode;
 import com.genome2d.particles.GParticleEmitter;
 import com.genome2d.textures.GTextureManager;
 
+
 class ParticlesExample extends AbstractExample
 {
+
+	private var emitter:GParticleEmitter;
 
     static public function main() {
         var inst = new ParticlesExample();
@@ -44,26 +49,45 @@ class ParticlesExample extends AbstractExample
 		var xml:Xml = Xml.parse('<particle_emitter enableSph="false" useWorldSpace="true" emit="true" delay="0" delayVariance="0" duration="2" loop="true" durationVariance="0" texture="@assets/atlas.png_particle"><p:rate><GCurve path="400"/></p:rate><ParticleModule enabled="true" spawnModule="true" updateModule="true"/></particle_emitter>');
 		var emitter:GParticleEmitter = GPrototypeFactory.createPrototype(GXmlPrototypeParser.fromXml(xml));
 		/**/
-		var emitter:GParticleEmitter = new GParticleEmitter();
+		emitter = new GParticleEmitter();
 		emitter.texture = GTextureManager.getTexture("assets/atlas.png_particle");
-		emitter.rate = new GCurve(200);
-		emitter.duration = 10;
+		//emitter.rate = new GCurve(532);
+		emitter.duration = 1;
 		emitter.loop = true;
-		var script:GScript = GScriptManager.createScript("script", GStaticAssetManager.getTextAssetById("assets/script.hxs").text);
-		var module:GScriptModule = new GScriptModule();
-		module.script = script;
+		emitter.enableSph = true;
+		var module:ParticleModule = new ParticleModule();
+		module.texture = GTextureManager.getTexture("assets/atlas.png_0");
+		emitter.burstDistribution = [.5, module.getParticleCount()];
 		emitter.addModule(module);
+		emitter.addModule(new GSPHVelocityModule());
 
 		// Create a node with simple particle system component
         var particleSystem:GParticleSystemComponent = GNode.createWithComponent(GParticleSystemComponent);
+		particleSystem.getParticleSystem().enableSph = true;
+		particleSystem.getParticleSystem().setupGrid(new GRectangle(0,0,800,600),20);
 		particleSystem.addEmitter(emitter);
 		particleSystem.node.setPosition(400, 300);
 		container.addChild(particleSystem.node);
+
+		Delta.tween(null).wait(.5).onComplete(onComplete_handler);
 
 		//container.postProcess = cast GXmlPrototypeParser.createPrototypeFromXmlString('<GBlurPP blurY="8" blurX="8"><p:bounds><GRectangle width="800" x="0" height="600" y="0"/></p:bounds></GBlurPP>');
 		//container.postProcess = new GBlurPP();
 		//container.postProcess.setBounds(new GRectangle(0,0,800,600));
 
 		//trace(GXmlPrototypeParser.toXml(container.getPrototype()));
+		genome.onUpdate.add(update_handler);
     }
+
+	private function update_handler(p_deltaTime:Float):Void {
+		Delta.step(p_deltaTime);
+	}
+
+	private function onComplete_handler():Void {
+		cast (emitter.getModules()[0],ParticleModule).fixed = Math.random()>.4;
+		emitter.x = Math.random()*800;
+		emitter.y = Math.random()*600;
+
+		Delta.tween(null).wait(.9).onComplete(onComplete_handler);
+	}
 }
--- src/com/genome2d/examples/custom/ParticleModule.hx
+++ src/com/genome2d/examples/custom/ParticleModule.hx
@@ -1,4 +1,6 @@
 package com.genome2d.examples.custom;
+import flash.geom.Point;
+import flash.display.BitmapData;
 import com.genome2d.geom.GCurve;
 import com.genome2d.particles.modules.GParticleEmitterModule;
 import com.genome2d.context.GBlendMode;
@@ -13,31 +15,70 @@ import com.genome2d.textures.GTexture;
  */
 class ParticleModule extends GParticleEmitterModule
 {
-	private var g2d_offset:Float = 0;
-	private var g2d_curveX:GCurve;
-	private var g2d_curveY:GCurve;
+	private var g2d_bitmapData:BitmapData;
+	private var g2d_texture:GTexture;
+	public var texture(default,set):GTexture;
+	public function set_texture(p_value:GTexture):GTexture {
+		g2d_texture = p_value;
+		if (Std.is(g2d_texture.getSource(),BitmapData)) g2d_bitmapData = cast g2d_texture.getSource();
+		if (Std.is(g2d_texture.getSource(),GTexture)) {
+			g2d_bitmapData = new BitmapData(Std.int(g2d_texture.region.width), Std.int(g2d_texture.region.height),true,0x0);
+			g2d_bitmapData.copyPixels(cast (g2d_texture.getSource(),GTexture).getSource(), g2d_texture.region.toNative(), new Point());
+		}
+		return g2d_texture;
+	}
 
-	public var distribution:Float = .1;
+	private var g2d_offset:Int = 0;
+	public var treshold:Float = 0;
+	public var gridSize:Int = 4;
+	public var fixed:Bool = true;
 
-	public function new(p_curveX:GCurve, p_curveY:GCurve) {
+	public function new() {
 		super();
 
-		g2d_curveX = p_curveX;
-		g2d_curveY = p_curveY;
-
 		spawnModule = true;
-		//updateModule = true;
+		updateModule = true;
+	}
+
+	public function getParticleCount():Int {
+		var count:Int = 0;
+		while (Std.int((g2d_offset*gridSize)/g2d_bitmapData.width)*gridSize < g2d_bitmapData.height) {
+			var color:UInt = g2d_bitmapData.getPixel32(Std.int(g2d_offset*gridSize)%g2d_bitmapData.width,Std.int(Std.int((g2d_offset*gridSize)/g2d_bitmapData.width)*gridSize));
+			var alpha:Float = (color >> 24 & 0xFF) / 0xFF;
+			if (alpha>treshold) count++;
+
+			g2d_offset++;
+		}
+
+		return count;
 	}
 	
 	override public function spawn(p_emitter:GParticleEmitter, p_particle:GParticle):Void {
-		p_particle.x = g2d_curveX.calculate(g2d_offset);
-		p_particle.y = g2d_curveY.calculate(g2d_offset);
+		var color:UInt = g2d_bitmapData.getPixel32(Std.int(g2d_offset*gridSize)%g2d_bitmapData.width,Std.int(Std.int((g2d_offset*gridSize)/g2d_bitmapData.width)*gridSize));
+		var alpha:Float = (color >> 24 & 0xFF) / 0xFF;
 
-		g2d_offset += distribution;
-		g2d_offset %= 1;
+		while (alpha<=treshold) {
+			g2d_offset++;
+			if (Std.int((g2d_offset*gridSize)/g2d_bitmapData.width)*gridSize > g2d_bitmapData.height) g2d_offset = 0;
+			color = g2d_bitmapData.getPixel32(Std.int(g2d_offset*gridSize)%g2d_bitmapData.width,Std.int(Std.int((g2d_offset*gridSize)/g2d_bitmapData.width)*gridSize));
+			alpha = (color >> 24 & 0xFF) / 0xFF;
+		}
+
+		p_particle.x = p_emitter.x + (g2d_offset*gridSize)%g2d_bitmapData.width - g2d_bitmapData.width/2;
+		p_particle.y = p_emitter.y + Std.int((g2d_offset*gridSize)/g2d_bitmapData.width)*gridSize - g2d_bitmapData.height/2;
+		p_particle.fixed = fixed;
+
+		g2d_offset++;
+		if (Std.int((g2d_offset*gridSize)/g2d_bitmapData.width)*gridSize > g2d_bitmapData.height) g2d_offset = 0;
 	}
 	
 	override public function update(p_emitter:GParticleEmitter, p_particle:GParticle, p_deltaTime:Float):Void {
+		p_particle.x += p_deltaTime/1000*p_particle.velocityX;
+		p_particle.y += p_deltaTime/1000*p_particle.velocityY;
 
+		if (!p_particle.fixed) p_particle.velocityY += 9;
+
+		p_particle.accumulatedTime += p_deltaTime;
+		if (p_particle.accumulatedTime > 15000) p_particle.die = true;
 	}
 }
